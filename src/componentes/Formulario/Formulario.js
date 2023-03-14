2c3edbbe9b43a423fbd1eb9fd4c25148808a92cb
lucianogomes02 <luciano@pontotel.com.br>
2023-03-13 23:41:08 -0300

    feat(src/componentes/ListaSuspensa.js): implementação inicial do componente ListaSuspensa

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,7 +1,18 @@
-import CampoTexto from "../CampoTexto/CampoTexto";
-import "./Formulario.css";
+import CampoTexto from "../CampoTexto/CampoTexto"
+import "./Formulario.css"
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão',
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,9 +20,10 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label="Time" items={times}/>
             </form>
         </section>
     )
 }
 
-export default Formulario;
+export default Formulario
