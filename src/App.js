2c68a0f24deaaaab4b8e165a8c7cdd4f57338a6a
lucianogomes02 <luciano@pontotel.com.br>
2023-03-21 00:38:43 -0300

    feat(src/componentes/Time/Time.js): implementação inicial do componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario/Formulario";
 import {useState} from "react";
+import Time from "./componentes/Time/Time";
 
 function App() {
     const [colaboradores, setColaboradores] = useState([])
@@ -11,6 +12,8 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-end"/>
     </div>
   );
 }
