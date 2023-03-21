981bd951c59e19906c6eddb3d501d6668862eabe
lucianogomes02 <luciano@pontotel.com.br>
2023-03-21 00:18:28 -0300

    refactor(App.js): adiciona colaborador ao state colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario/Formulario";
+import {useState} from "react";
 
 function App() {
+    const [colaboradores, setColaboradores] = useState([])
+    const aoNovoColaboradorAdicionado = (colaborador) => {
+        setColaboradores([...colaboradores, colaborador])
+    }
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -5,7 +5,7 @@ import Botao from "../Botao/Botao";
 
 import {useState} from "react";
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-end',
@@ -23,7 +23,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log("Formulário submetido =>", nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado(
+            nome,
+            cargo,
+            imagem,
+            time
+        )
     }
 
     return (
