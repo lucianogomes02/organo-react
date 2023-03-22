49ff6c39ec50426f00b839f8b881b8d5ba2c1bc4
lucianogomes02 <luciano@pontotel.com.br>
2023-03-21 22:15:55 -0300

    feat(src/componentes/Time/Time.js): implementa mudarCorDoTime

--- src/App.js
+++ src/App.js
@@ -5,43 +5,45 @@ import Time from "./componentes/Time/Time";
 
 function App() {
 
-    const times  = [
-        {
-            nome: "Programação",
-            corPrimaria: "#57C278",
-            corSecundaria: "#D9F7E9"
-        },
-        {
-            nome: "Front-end",
-            corPrimaria: "#82CFFA",
-            corSecundaria: "#E8F8FF"
-        },
-        {
-            nome: "Data Science",
-            corPrimaria: "#A6D157",
-            corSecundaria: "#F0F8E2"
-        },
-        {
-            nome: "DevOps",
-            corPrimaria: "#E06B69",
-            corSecundaria: "#FDE7E8"
-        },
-        {
-            nome: "UX e Design",
-            corPrimaria: "#DB6EBF",
-            corSecundaria: "#FAE9F5"
-        },
-        {
-            nome: "Mobile",
-            corPrimaria: "#FFBA05",
-            corSecundaria: "#FFF5D9"
-        },
-        {
-            nome: "Inovação e Gestão",
-            corPrimaria: "#FF8A29",
-            corSecundaria: "#FFEEDF"
-        },
-    ]
+    const [times, setTimes]  = useState(
+        [
+            {
+                nome: "Programação",
+                corPrimaria: "#57C278",
+                corSecundaria: "#D9F7E9"
+            },
+            {
+                nome: "Front-end",
+                corPrimaria: "#82CFFA",
+                corSecundaria: "#E8F8FF"
+            },
+            {
+                nome: "Data Science",
+                corPrimaria: "#A6D157",
+                corSecundaria: "#F0F8E2"
+            },
+            {
+                nome: "DevOps",
+                corPrimaria: "#E06B69",
+                corSecundaria: "#FDE7E8"
+            },
+            {
+                nome: "UX e Design",
+                corPrimaria: "#DB6EBF",
+                corSecundaria: "#FAE9F5"
+            },
+            {
+                nome: "Mobile",
+                corPrimaria: "#FFBA05",
+                corSecundaria: "#FFF5D9"
+            },
+            {
+                nome: "Inovação e Gestão",
+                corPrimaria: "#FF8A29",
+                corSecundaria: "#FFEEDF"
+            },
+        ]
+    )
 
     const [colaboradores, setColaboradores] = useState([])
 
@@ -51,6 +53,18 @@ function App() {
 
     function deletarColaborador() {
         console.log('Colaborador deletado')
+    }
+
+    function mudarCorDoTime(cor, nome) {
+        setTimes(times.map(time =>
+                {
+                    if (time.nome === nome) {
+                        time.corPrimaria = cor;
+                    }
+                    return time
+                }
+            )
+        )
     }
 
   return (
@@ -74,6 +88,7 @@ function App() {
                     colaborador => colaborador.time === time.nome
                 )
             }
+            mudarCor={mudarCorDoTime}
             aoDeletar={deletarColaborador}
       />)}
     </div>
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,14 +1,14 @@
 import './Time.css'
 import Colaborador from "../Colaborador/Colaborador";
 
-const Time = ({ colaboradores, nome, corPrimaria, corSecundaria, aoDeletar }) => {
+const Time = ({ colaboradores, nome, corPrimaria, corSecundaria, mudarCor, aoDeletar }) => {
     const cssSection = { backgroundColor: corSecundaria }
     const cssH3 = {borderColor: corPrimaria}
 
     return (
         colaboradores.length > 0 &&
         <section className="time" style={cssSection}>
-            <input type='color' className='input-cor' value={corSecundaria} />
+            <input type='color' className='input-cor' value={corSecundaria} onChange={evento => mudarCor(evento.target.value, nome)}/>
             <h3 style={cssH3}>{nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) =>
